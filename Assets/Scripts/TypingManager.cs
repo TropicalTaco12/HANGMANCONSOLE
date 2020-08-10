6e38faf4f27c3259832c64beb71807adeaeb68fb
Aaron <aling0703@gmail.com>
2020-08-10 14:51:59 -0700

    Got the scaffold with Justin

---
+++ Assets/Scripts/TypingManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TypingManager : MonoBehaviour
+{
+
+
+    public List<word> words;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
+
+
+
+[System.Serializable]
+public class word
+{
+    public string text;
+    public UnityEvent onTyped;
+    string hasTyped;
+    int currentChar;
+    int number = 3;
+
+    public word(string t)
+    {
+        text = t;
+        hasTyped = "";
+        currentChar = 0;
+    }
+
+
+    public bool contintueText(char c)
+}
+
