5fa107b54963354a1404728838bdc3c9f6f4cb11
Aaron <aling0703@gmail.com>
2020-08-13 12:58:55 -0700

    Incorrect funtion is working.

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ using System.Collections.Generic;
 
 namespace HangMan
 {
-    
+
     class Program
     {
 
 
-        
-       
+
+
         protected static int origRow;
         protected static int origCol;
         protected static void WriteAt(string s, int col, int row)
@@ -34,29 +34,26 @@ namespace HangMan
 
         public static void Main(string[] args)
         {
-            origCol = 3;
-            origRow = 3;
-            int limbs = 3;
-            HangManGenerator(5);
+            Guess();
 
+            Console.WriteLine("Welcome to Hangman. Enter ur Guess");
             //ShowMainMenu();
             //OnUserInput();
         }
 
         public static void HangManGenerator(int limbs)
         {
-            
+
 
             Scaffold();
 
 
-           
+
 
             if (limbs == 1)
             {
-
                 WriteAt("O", 5, 2);
-                Scaffold();
+                //Scaffold();
             }
 
 
@@ -66,7 +63,7 @@ namespace HangMan
                 WriteAt("O", 5, 2);
                 WriteAt("|", 5, 3);
 
-                Scaffold();
+                //Scaffold();
             }
 
 
@@ -75,8 +72,8 @@ namespace HangMan
                 WriteAt("O", 5, 2);
                 WriteAt("|", 5, 3);
                 WriteAt("\\", 6, 3);
-                WriteAt("/", 4, 3);
-                Scaffold();
+                
+                //Scaffold();
             }
 
 
@@ -85,9 +82,9 @@ namespace HangMan
                 WriteAt("/", 4, 3);
                 WriteAt("O", 5, 2);
                 WriteAt("|", 5, 3);
-                WriteAt("\\", 6, 3);
+                
                 WriteAt("/", 4, 3);
-                Scaffold();
+                //Scaffold();
             }
 
 
@@ -99,7 +96,7 @@ namespace HangMan
                 WriteAt("\\", 6, 3);
                 WriteAt("/", 4, 3);
                 WriteAt("/", 4, 4);
-                Scaffold();
+                //Scaffold();
             }
 
             if (limbs == 6)
@@ -111,113 +108,18 @@ namespace HangMan
                 WriteAt("/", 4, 3);
                 WriteAt("/", 4, 4);
                 WriteAt("\\", 6, 4);
-                Scaffold();
+                //Scaffold();
             }
 
 
-            //drawHangMan[0] = "O";
-            //drawHangMan[1] = "|";
-            //drawHangMan[2] = "/";
-            //drawHangMan[3] = "\\";
-            //drawHangMan[4] = "/";
-            //drawHangMan[5] = "\\";
-            //drawHangMan[6] = "-";
-            //drawHangMan[7] = "+";
 
-            //WriteAt(drawHangMan[1], 3, 2);
-            //WriteAt(drawHangMan[7], 3, 1);
-            //WriteAt(drawHangMan[6], 1, 1);
-            //WriteAt(drawHangMan[6], 0, 1);
-            //WriteAt(drawHangMan[1], )
-
-            //WriteAt(drawHangMan[0], 3, 3);
-            //WriteAt(drawHangMan[1], 3, 4);
-            //WriteAt(drawHangMan[2], 2, 4);
-            //WriteAt(drawHangMan[3], 4, 4);
-            //WriteAt(drawHangMan[4], 2, 5);
-            //WriteAt(drawHangMan[5], 4, 5);
-
-
-
-            //WriteAt("O", 3, 3);
-            //WriteAt("|", 3, 4);
-            //WriteAt("/", 2, 4);
-            //WriteAt("\\", 4, 4);
-            //WriteAt("/", 2, 5);
-            //WriteAt("\\", 4, 5);
 
         }
-
-        public static void ShowMainMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("Welcome To Hangman made by Only Justin");
-            Console.WriteLine("Select a difficulty:");
-            Console.WriteLine("1 for Easy");
-            Console.WriteLine("2 for Meduim");
-            Console.WriteLine("3 for HARD");
-        }
-
-
-        public static void OnUserInput()
-        {
-
-            string personInput = Console.ReadLine();
-            
-
-
-            if (personInput == "1")
-            {
-                int level = 1;
-                Console.WriteLine("You have selected level: ", level);
-            }
-            else if (personInput == "2")
-            {
-                int level = 2;
-                Console.WriteLine("You have selected level: ", level);
-            }
-            else if (personInput == "3")
-            {
-                int level = 3;
-                Console.WriteLine("You have selected level: ", level);
-            }
-
-            if (personInput == "menu")
-            {
-                ShowMainMenu();
-            }
-
-        }
-        
-        public static void LevelOne()
-        {
-            var random = new Random();
-            var list = new List<string> { "Gerald", "Angela", "Justin", "Aaron" };
-            int index = random.Next(list.Count-1);
-            Console.WriteLine(list[index]);
-
-        }
-
-
-        public static void LevelTwo()
-        {
-            var random = new Random();
-            var list = new List<string> { "Paschal", "Nicholas", "Josephine", "Elizabeth" };
-            int index = random.Next(list.Count-1);
-            Console.WriteLine(list[index]);
-
-        }
+        //int index = random.next(list.count - 1);
+        //TODO
 
 
 
-        public static void LevelThree()
-        {
-            var random = new Random();
-            var list = new List<string> { "Annabella", "Isabella", "Andy Anderson the Second", "Christopher" };
-            int index = random.Next(list.Count-1);
-            Console.WriteLine(list[index]);
-
-        }
 
         public static void Scaffold()
         {
@@ -228,23 +130,149 @@ namespace HangMan
             drawHangMan[2] = "-";
 
 
-            WriteAt(drawHangMan[0], 5, 1);
-            WriteAt(drawHangMan[1], 0, 0);
-            WriteAt(drawHangMan[0], 0, 1);
-            WriteAt(drawHangMan[0], 0, 2);
-            WriteAt(drawHangMan[0], 0, 3);
-            WriteAt(drawHangMan[0], 0, 4);
-            WriteAt(drawHangMan[2], 0, 5);
-            WriteAt(drawHangMan[0], 0, 5);
-            WriteAt(drawHangMan[2], 1, 5);
-            WriteAt(drawHangMan[2], 2, 5);
-            WriteAt(drawHangMan[2], 3, 5);
-            WriteAt(drawHangMan[2], 4, 5);
-            WriteAt(drawHangMan[2], 5, 5);
-            WriteAt(drawHangMan[2], 6, 5);
+            Console.WriteLine(drawHangMan[0], 5, 1);
+            Console.WriteLine(drawHangMan[1], 0, 0);
+            Console.WriteLine(drawHangMan[0], 0, 1);
+            Console.WriteLine(drawHangMan[0], 0, 2);
+            Console.WriteLine(drawHangMan[0], 0, 3);
+            Console.WriteLine(drawHangMan[0], 0, 4);
+            Console.WriteLine(drawHangMan[2], 0, 5);
+            Console.WriteLine(drawHangMan[0], 0, 5);
+            Console.WriteLine(drawHangMan[2], 1, 5);
+            Console.WriteLine(drawHangMan[2], 2, 5);
+            Console.WriteLine(drawHangMan[2], 3, 5);
+            Console.WriteLine(drawHangMan[2], 4, 5);
+            Console.WriteLine(drawHangMan[2], 5, 5);
+            Console.WriteLine(drawHangMan[2], 6, 5);
 
-           
+            
         }
+
+
+
+
+        //public static void GuessCharacter(int limbs)
+        //{
+        //    string word = "gerald";
+        //    char[] letter = word.ToCharArray();
+
+
+        //    string x = Console.ReadLine();
+        //    char personInput = Convert.ToChar(x);
+
+
+        //    for (int i = 0; i < 6; i++)
+        //    {
+        //        if (personInput != letter[0])
+        //        {
+        //            Console.WriteLine("Incorrect: {0}", personInput);
+        //            limbs++;
+        //        }
+        //        if (personInput == letter[i])
+        //        {
+
+        //            Console.WriteLine(personInput);
+        //            Console.WriteLine("Correct!");
+        //        }
+        //        for (int j = 0; j < 1; j++)
+        //        {
+        //            x = Console.ReadLine();
+        //        }
+        //    }
+
+
+        public static void Guess()
+        {
+            int limbs = 0;
+            HangManGenerator(limbs);
+            var random = new Random();
+            var list = new List<string> { "gerald", "aaron", "justin", "angela" };
+            int index = random.Next(list.Count);
+
+            //creates remain array
+            char[] remain = list[index].ToCharArray();
+            //creates result array
+            char[] result = new char[remain.Length];
+
+            while (true)
+            {
+
+
+                
+
+
+                string x = Console.ReadLine();
+
+                char personInput = Convert.ToChar(x);
+
+                //prints g e r a l d reamins
+                for (int i = 0; i < remain.Length; i++)
+                {
+                    Console.Write("" + remain[i]);
+                }
+                Console.Write("  ");
+
+
+                //prints  the resukt
+                //for (int i = 0; i < remain.Length; i++)
+                //{
+                //    Console.Write("  ");
+                //    Console.Write("  " + result[i]);
+
+                //}
+                //Console.WriteLine(personInput);
+                //Console.Write(" ");
+                //Console.Write("");
+
+                //the process of switching remian to result
+                var guessedCorrect = false;
+
+                for (int i = 0; i < remain.Length; i++)
+                {
+                    if (personInput == remain[i])
+                    {
+                        char temp = remain[i];
+                        remain[i] = ' ';
+                        result[i] = temp;
+                        guessedCorrect = true;
+                    }
+                }
+
+                if (! guessedCorrect)
+                {
+                    limbs++;
+                }
+
+                for (int i = 0; i < remain.Length; i++)
+                {
+                    Console.Write("  ");
+                    Console.Write("  " + result[i]);
+
+                }
+                Console.WriteLine(personInput);
+                Console.Write(" ");
+                Console.Write("");
+
+                HangManGenerator(limbs);
+
+                if (limbs == 7)
+                {
+                    LoseScreen();
+                }
+            }
+
+        }
+
+        public static void LoseScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("YOU LOSE. Type menu to go back to the main screen");
+        }
+
 
     }
 }
+
+
+
+
